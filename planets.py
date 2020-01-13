5ea6ef896229b149f3e2f31713041254651ea94e
Lauren Riddle <lari7132@gmail.com>
2020-01-13 13:55:18 -0600

    Use the extend() method to add another list of the last two planets in our solar system to the end of the list.

--- planets.py
+++ planets.py
@@ -1,5 +1,7 @@
 planet_list = ["Mercury", "Mars"]
 
-planet_list.append("Jupiter")
-planet_list.append("Saturn")
+# planet_list.append("Jupiter")
+# planet_list.append("Saturn")
+
+planet_list.extend(["Jupiter", "Saturn"])
 print(planet_list)
