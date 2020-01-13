efc7a0bc870706558770defa852f9614cbed72f4
Lauren Riddle <lari7132@gmail.com>
2020-01-13 13:59:51 -0600

    Use insert() to add Earth, and Venus in the correct order.

--- planets.py
+++ planets.py
@@ -3,5 +3,8 @@ planet_list = ["Mercury", "Mars"]
 # planet_list.append("Jupiter")
 # planet_list.append("Saturn")
 
-planet_list.extend(["Jupiter", "Saturn"])
+# planet_list.extend(["Jupiter", "Saturn"])
+planet_list.insert(2, "Earth")
+planet_list.insert(3, "Venus")
+
 print(planet_list)
