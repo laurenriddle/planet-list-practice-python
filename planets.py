3b8b7525fb6ad25f5c852907335d63882b262974
Lauren Riddle <lari7132@gmail.com>
2020-01-13 14:03:41 -0600

    Now that all the planets are in the list, slice the list in order to get the rocky planets into a new list called rocky_planets.

--- planets.py
+++ planets.py
@@ -4,7 +4,9 @@ planet_list = ["Mercury", "Mars"]
 # planet_list.append("Saturn")
 
 planet_list.extend(["Jupiter", "Saturn"])
-planet_list.insert(4, "Earth")
-planet_list.insert(5, "Venus")
+planet_list.insert(2, "Earth")
+planet_list.insert(3, "Venus")
 planet_list.append("Pluto")
 print(planet_list)
+rocky_planets = planet_list[0:4]
+print(rocky_planets)
